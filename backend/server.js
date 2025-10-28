12d4e4647cc3d0211f39f28d4d9c6506e7d207e0
Sheryl Jepkosgei <jepkosgeisheryl19@gmail.com>
2025-10-28 13:29:17 +0300

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
